a8d0d7fec608981c317445ddc22e835c31532c3a
Attiq <0703thuy@gmail.com>
2022-09-21 09:36:01 +0700

    create page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import HomePage from './Pages/HomePage/HomePage';
+
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          Nhat tao branch
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+
+        <Route path="/home" element={<HomePage />} />
+
+        
+      </Routes>
+    </>
   );
 }
 
---
+++ src/Pages/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>Login
+      <div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Login
