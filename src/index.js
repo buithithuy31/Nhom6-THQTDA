91e7f202cf1ba7e8e510e7bd4979f5526606dae7
tranhung1705 <85281469+tranhung1705@users.noreply.github.com>
2022-09-25 23:20:15 +0700

    m

--- src/index.js
+++ src/index.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import '../node_modules/font-awesome/css/font-awesome.min.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import 'react-loading-skeleton';
-import 'react-bootstrap';
-=======
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
@@ -22,7 +8,6 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './redux/Store'
->>>>>>> 879aad440acac9904be31b960d0168e14f90cd77
 
 ReactDOM.render(
   <BrowserRouter>
