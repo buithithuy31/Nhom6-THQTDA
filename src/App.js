2c67beaae54f9d822a10f8d176073afd52f8569c
Attiq <0703thuy@gmail.com>
2022-09-25 22:50:34 +0700

    trangchu

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ProductPage from "./Pages/ProductPage";
 import {Login} from './Pages/Login'
 import { Container } from "react-bootstrap";
 import { Routes, Route } from "react-router-dom";
-import Register from "./Pages/Register";
+import {Register} from "./Pages/Register";
 import Header from "./Components/Header";
 // import "react-loading-skeleton/dist/skeleton.css";
 import "react-loading-skeleton"
@@ -14,17 +14,17 @@ function App() {
   return (
     <>
     <div>
-    <Login/>
+    {/* <Login/> */}
     </div>
-      {/* <Header/> */}
-      {/* <Container fluid className="p-0" style={{ marginTop: "80px" }}>
+      <Header/>
+      <Container fluid className="p-0" style={{ marginTop: "80px" }}>
         <Routes>
           <Route index path="/" element={<HomePage />} />
           <Route path="/product" element={<ProductPage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
-      </Container> */}
+      </Container>
       
     </>
   );
